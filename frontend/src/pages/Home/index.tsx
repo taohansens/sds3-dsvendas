827f0af5b590e332b40502013fd80ba765a39b9a
taohansens <taohansens@gmail.com>
2021-05-09 11:50:42 -0300

    Finish Home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -13,8 +13,8 @@ function Home() {
                     <hr />
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                     <Link className="btn btn-primary btn-lg" to="/dashboard">
-                        Acessa dashboard
-                </Link>
+                        Acessar dashboard
+                    </Link>
                 </div>
             </div>
             <Footer />
